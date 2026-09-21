c0cd7375710270fee773e7f05475ab5ae262c7f8
Ridho Kamaal <ridhokamal16@gmail.com>
2026-09-21 15:50:28 +0800

    commit pertama

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/HaloPBO2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class HaloPBO2 {
+    public static void main(String[] args) {
+String nama = "Mahasiswa Teknik Informatika";
+System.out.println("Halo, " + nama + "!");
+System.out.println("Selamat datang di Pemrograman Berbasis Objek 2.");
+System.out.println();
+// Informasi lingkungan Java yang sedang digunakan
+System.out.println("Versi Java : " + System.getProperty("java.version"));
+System.out.println("Vendor JDK : " + System.getProperty("java.vendor"));
+System.out.println("Sistem operasi : " + System.getProperty("os.name"));
+}
+}
+    
+
---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class KartuMahasiswa {
+     public static void main(String[] args) {
+
+        // Data mahasiswa
+        String nama = "Ridho Kamaal";
+        String npm = "2410010059";
+        String prodi = "Teknik Informatika";
+        int semester = 5;
+        String alasan = "Ingin memahami pemrograman berorientasi objek";
+
+        // Menampilkan kartu mahasiswa
+        System.out.println("================================");
+        System.out.println("       KARTU MAHASISWA PBO 2");
+        System.out.println("================================");
+        System.out.println("Nama      : " + nama);
+        System.out.println("NPM       : " + npm);
+        System.out.println("Prodi     : " + prodi);
+        System.out.println("Semester  : " + semester);
+        System.out.println("Alasan    : " + alasan);
+        System.out.println("================================");
+    }
+    
+}
